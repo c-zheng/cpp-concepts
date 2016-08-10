5608bd1d04a3d86af3df27a9bac7ed65a054d74d
Chen Zheng <chen.zheng88@yahoo.com>
2016-08-09 22:58:02 -0700

    Basic example of shared pointer

---
+++ smart_ptr/shared_ptr/sharedptr.cpp
@@ -0,0 +1,42 @@
+#include <memory>
+#include <iostream>
+
+class Base{
+  public:
+    Base(){
+      std::cout << "Construct Base" << std::endl;
+    }
+    ~Base(){
+      std::cout << "Destroy Base" << std::endl;
+    }
+};
+
+int main(int argc, char ** argv)
+{
+  // Create with new
+  std::shared_ptr<Base> b0(new Base);
+  std::cout << "b0 use count " << b0.use_count() << std::endl;
+  // Create with copy ctor
+  std::shared_ptr<Base> b1(b0);
+  std::cout << "b0 use count " << b0.use_count() << std::endl;
+  // Create with assignment 
+  std::shared_ptr<Base> b2 = b1;
+  std::cout << "b0 use count " << b0.use_count() << std::endl;
+  // Create with std::make_shared
+  std::shared_ptr<Base> b3(std::make_shared<Base>());
+  std::cout << "b3 use count " << b3.use_count() << std::endl;
+  // Destroy with re-assignment, b3 goes out of scope
+  b3 = b2;
+  std::cout << "b0 use count " << b3.use_count() << std::endl;
+  // Destory with out of scope
+  {
+    // Create with std::make_shared
+    std::shared_ptr<Base> b4(std::make_shared<Base>());
+  }
+  // Destroy with assign to nullptr
+  b2 = nullptr;
+  std::cout << "b0 use count " << b3.use_count() << std::endl;
+  // Destroy with reset 
+  b1.reset();
+  std::cout << "b0 use count " << b3.use_count() << std::endl;
+}
